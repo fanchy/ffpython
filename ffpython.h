1f75384c70b594180120281b95fce9939e4f1d20
Evan Zhao <zxfown@gmail.com>
2013-06-19 23:26:02 -0700

    Py_IsInitialized

--- ffpython.h
+++ ffpython.h
@@ -391,6 +391,12 @@ class ffpython_t
 	};
 
 public:
+    ffpython_t()
+    {
+        if (!Py_IsInitialized())
+            Py_Initialize();
+    }
+
     static int init_py();
     static int final_py();
 	static int add_path(const string& path_);
