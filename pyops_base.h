91d59cdb940bfce2670fd3257a1d5c713af461f3
fanchy <zxfown@gmail.com>
2019-10-22 20:21:27 +0800

    fix linux compile error

--- pyops_base.h
+++ pyops_base.h
@@ -4,10 +4,10 @@
 
 #ifdef _DEBUG
   #undef _DEBUG
-  #include <python.h>
+  #include <Python.h>
   #define _DEBUG
 #else
-  #include <python.h>
+  #include <Python.h>
 #endif
 #include <structmember.h>
 
